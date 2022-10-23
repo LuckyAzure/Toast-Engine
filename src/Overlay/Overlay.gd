01f9ec416c52053ec73a6041355ee4f1a5d34e02
Lucky <baloghmarcellkrisztofer@gmail.com>
2022-10-23 12:49:50 +0200

    menu art wip

--- src/Overlay/Overlay.gd
+++ src/Overlay/Overlay.gd
@@ -3,11 +3,28 @@ extends CanvasLayer
 var Stats = {"FPS":0,"Memory":0,"Memory_Peak":0}
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_fullscreen"):
-		OS.window_fullscreen = !OS.window_fullscreen
+	# Changes the "Stats" variables 
 	Stats.FPS = Engine.get_frames_per_second()
 	Stats.Memory = stepify(OS.get_static_memory_usage() / 1048576.0,0.01)
 	Stats.Memory_Peak = stepify(OS.get_static_memory_peak_usage() / 1048576.0,0.01)
-	$Text.text = "FPS: " + str(Stats.FPS)
-	$Text.text += "\nMemory: " + str(Stats.Memory) + " MB"
-	$Text.text += "\nMemory Peak: " + str(Stats.Memory_Peak) + " MB"
+	
+	# Important Functions
+	_Fullscreen()
+	_DebugStatus()
+
+# ------------------------------------------------------------------------
+
+func _Fullscreen():
+	# Toggles Fullscreen
+	if Input.is_action_just_pressed("ui_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+# ------------------------------------------------------------------------
+
+func _DebugStatus():
+	$Debug.text = "" # Clears text
+	$Debug.text += "FPS: " + str(Stats.FPS) # Frames per Second
+	$Debug.text += "\nMemory: " + str(Stats.Memory) + " MB" # Memory Usage
+	$Debug.text += "\nMemory Peak: " + str(Stats.Memory_Peak) + " MB" # Max Memory Used
+
+# ------------------------------------------------------------------------
--- src/Scenes/Menu/Menu.gd
+++ src/Scenes/Menu/Menu.gd
@@ -14,6 +14,8 @@ func _ready():
 	var texture = ImageTexture.new()
 	texture.create_from_image(BG)
 	$Background.texture = texture
+	
+	_reloadchar()
 
 func _process(delta):
 	if !selected:
@@ -23,15 +25,19 @@ func _process(delta):
 
 func _Input():
 	if Input.is_action_just_pressed("ui_up"):
+		$Char.position.y = 1200
 		if select > 0:
 			select -= 1
 		else:
 			select = menu_options.size() - 1
+		_reloadchar()
 	if Input.is_action_just_pressed("ui_down"):
+		$Char.position.y = 1200
 		if select < menu_options.size() - 1:
 			select += 1
 		else:
 			select = 0
+		_reloadchar()
 	if Input.is_action_just_pressed("ui_accept"):
 		ScreenEffects._flash()
 		selected = true
@@ -48,5 +54,15 @@ func _Animations(delta):
 		else:
 			get_node(menu_options[select]).modulate.a = lerp(get_node(menu_options[select]).modulate.a,1,20 * delta)
 	if selected:
-		get_node(menu_options[select]).rect_position.x = lerp(get_node(menu_options[select]).rect_position.x,400,10 * delta)
+		get_node(menu_options[select]).rect_position.x = lerp(get_node(menu_options[select]).rect_position.x,50,10 * delta)
+		get_node(menu_options[select]).rect_position.y = lerp(get_node(menu_options[select]).rect_position.y,320,10 * delta)
 		get_node(menu_options[select]).rect_rotation = lerp(get_node(menu_options[select]).rect_rotation,0,10 * delta)
+		$Design/Border.rect_position.x = lerp($Design/Border.rect_position.x,-360,10 * delta)
+	$Char.position.y = lerp($Char.position.y,360,10 * delta)
+
+func _reloadchar():
+	var Char = Image.new()
+	Char.load("res://assets/images/" + menu_options[select] + ".png")
+	var ctexture = ImageTexture.new()
+	ctexture.create_from_image(Char)
+	$Char.texture = ctexture
